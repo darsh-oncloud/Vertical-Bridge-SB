ee99d54f8bd87a38582a0099384bbed0cc06f118
darsh-oncloud <dkothari@oncloudconsulting.ca>
2026-09-22 14:05:09 -0400

    Update from NetSuite (Auto)

--- VB_Send_Vendor_detail.js
+++ VB_Send_Vendor_detail.js
@@ -134,7 +134,6 @@ define(['N/search', 'N/https'], (search, https) => {
                 url: 'https://apdev.verticalbridge.com/VendorApi/vendor',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Accept': 'application/json',
                     'x-api-key': 'Vbd39KmpZLzm2ldP8qxSA67bmKAcjWotfNczVFxV4rI'
                 },
                 body: payloadString
